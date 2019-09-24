fa73780a79a562701b6ac59d3bcbc548cf775581
unknown <leoknaw@gmail.com>
2019-09-24 22:25:17 +0100

    Add

--- scrape/urls.py
+++ scrape/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path("", views.home, name="home"),
+    path("search/", views.newSearch, name="newSearch"),
 ]
--- scrape/views.py
+++ scrape/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 
 def home(request):
-    return render(request, "scrape/base.html")
+    return render(request, "base.html")
+
+
+def newSearch(request):
+    search = request.POST.get('search')
+   # print(search)
+    frontend = {
+        'search':search,
+                }
+    return render(request, "scrape/newSearch.html", frontend)
