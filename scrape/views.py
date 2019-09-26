fcb7eead76757a326c998eaa1a34ec820a733bc1
unknown <leoknaw@gmail.com>
2019-09-26 08:14:31 +0100

    Added functionality

--- scrape/views.py
+++ scrape/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from requests.compat import quote_plus
+from .models import Search
 # Create your views here.
+
+BASE_URL = 'https://accra.craigslist.org/search/?query={}'
+BASE_IMG = 'https://images.craigslist.org/{}_300x300.jpg'
 
 def home(request):
     return render(request, "base.html")
@@ -9,8 +14,38 @@ def home(request):
 
 def newSearch(request):
     search = request.POST.get('search')
-   # print(search)
+    Search.objects.create(search=search)
+    url = BASE_URL.format(quote_plus(search))
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, features='html.parser')
+
+    scrape = soup.find_all('li', {'class':'result-row'})
+
+
+    finalPost = []
+
+
+    for post in scrape:
+        scrapeTitle = post.find(class_='result-title').text
+        scrapeURL = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            scrapePrice = post.find(class_='result-price').text
+        else:
+            scrapePrice = 'N/A'
+
+        if post.find(class_='result-image gallery') is not None:
+            scrapeId = post.find(class_='result-image gallery').get('data-ids').split(',')[0].split(':')[1]
+            scrapeImg = BASE_IMG.format(scrapeId)
+        elif post.find(class_='result-image gallery') is None:
+            scrapeImg = 'https://accra.craigslist.org/images/peace.jpg'
+        finalPost.append((scrapeTitle, scrapeURL, scrapePrice, scrapeImg))
+
+
+
+
+    #print(url)
     frontend = {
         'search':search,
+        'finalPost': finalPost,
                 }
     return render(request, "scrape/newSearch.html", frontend)
