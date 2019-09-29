932da7cab32038e985313a99713c1567f969479a
unknown <leoknaw@gmail.com>
2019-09-29 13:45:40 +0100

    Requirements

--- craig/settings.py
+++ craig/settings.py
@@ -17,7 +17,6 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
